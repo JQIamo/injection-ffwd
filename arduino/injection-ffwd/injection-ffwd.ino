a282e4f23923d867d09bd9cd2b7f1bcf62196eb8
Neal Pisenti <npisenti@gmail.com>
2016-10-08 08:24:44 -0400

    initial code commit; mostly working
    * still need to implement display, rotary encoder
    * eventual integration over backplane?

---
+++ arduino/injection-ffwd/injection-ffwd.ino
@@ -0,0 +1,491 @@
+#include "DueTimer.h"
+#include "injection-init.h"
+
+/*
+
+// states
+r[0]: Reset state; DAC -> midscale; piezo scan is on but no FB
+l[1]: lock state; implements PID
+a[2]: acquire; implements re-acquire routine
+
+// parameters
+p -> interpret as parameter
+command structure: 'p 0 15' -> set param 0 (pzt_min) to 15
+
+0 pzt_min
+1 pzt_max
+2 scans_to_average
+3 reaq_stepsize
+4 fb_stepsize
+5 unlocked_threshold
+6 locked_threshold
+7 pzt_offset_delta
+*/
+
+
+volatile bool ramp_dir = 1; // 1 = rising; 0 = falling
+volatile bool end_scan_flag = 0;    // flag for end of single period of pzt scan
+
+uint16_t fb_o = 2048;   // current feedback output; set to mid-scale.
+
+
+int unlocked_threshold = 1250;
+int locked_threshold = 1450;
+int fb_stepsize = 30;
+int reaq_stepsize = 250;
+
+int pzt_offset_delta = 300;
+
+
+// state for state machine
+int state = 0;
+bool railed = 0;
+
+
+int scan_counter = 0;
+float average_height = 0.0;
+float global_average = 0.0;
+
+
+//lockmode_t fb_mode;
+
+int fb_flag = 0;
+
+
+uint16_t df_fb = 300;
+
+int lock_sm = 0;
+float lock_hist[3];
+uint16_t lock_val[3];
+
+
+
+
+
+
+// generate triangle ramp 
+void advance_waveform(){
+  pzt_o += (ramp_dir ? PZT_RRR : PZT_FRR);
+
+  // check bounds, correct if necessary and reverse ramp direction.
+  if (pzt_o >= pzt_max){
+    pzt_o = pzt_max;
+    ramp_dir = 0;
+  } else if (pzt_o < pzt_min){
+    pzt_o = pzt_min;
+    ramp_dir = 1;
+
+    Timer4.stop();
+    NVIC_DisableIRQ(ADC_IRQn);
+    end_scan_flag = 1;
+  }
+
+  analogWrite(DAC0, pzt_o);
+}
+
+void reset_waveform(){
+  pzt_o = pzt_min;
+  ramp_dir = 1;
+  end_scan_flag = 0;
+  pk_count = 0;
+}
+
+
+
+void query_cavity(){
+  reset_waveform(); // reset counters, buffers, etc.
+  NVIC_EnableIRQ(ADC_IRQn); // turn on ADC interrupts
+  Timer4.start();   // Timer4 is attached to advance_waveform; enable to start PZT waveform generation
+}
+
+
+/* State machine:
+
+Sc
+
+*/
+
+
+
+
+void setup() {
+
+  Serial.begin(115200);
+  
+  adc_dac_setup();  // configure ADC & DAC
+  
+  // output voltages on PZT and DAC
+  analogWrite(DAC0, pzt_min);
+  analogWrite(DAC1, fb_o);
+  
+  // attach timer interrupt for PZT waveform
+  Timer4.attachInterrupt(advance_waveform).setPeriod(PZT_TIMESTEP);
+  
+  // initialize state machine...?
+  state = 0;
+  // start
+  query_cavity();
+
+}
+
+
+
+
+
+void update_fb(){
+
+    // make sure we loop back to 4095
+    if (fb_o > 4095){
+        fb_o = 4095;
+    }
+
+
+    // if in lock mode and fb_o close to rail, throw rail flag
+    // else, loop at 12 bits
+    if (state == 1 &&  (((4095 - fb_o) < RAILED_OVERHEAD) || fb_o < RAILED_OVERHEAD)){
+        railed = 1;
+    } else {
+        railed = 0;
+    }
+    
+    analogWrite(DAC1, fb_o);    
+}
+
+
+void processSM(){
+
+   
+    switch (state){
+    case 0:
+        // state 0: reset
+        fb_o = 2048;
+        update_fb();
+        query_cavity();
+        break;
+    
+    case 1:
+        // state 1: lock mode
+        
+        
+        break;
+        
+    case 2:
+    
+        break;
+        
+    default:
+        fb_o = 2048;
+        update_fb();
+        query_cavity();
+        break;
+    }
+
+
+}
+
+void processSerial(){
+ // check for serial
+    if (Serial.available() > 0){
+        char c = Serial.read();
+        switch (c){
+        case 'r':
+            // reset state
+            state = 0;
+           //  Serial.print("state: ");
+//             Serial.println(state);
+            break;
+        case 'l':
+            // lock state
+            state = 1;
+//             Serial.print("state: ");
+//             Serial.println(state);            
+            break;
+        case 'a':
+            // lock state
+            state = 2;
+           //  Serial.print("state: ");
+//             Serial.println(state);           
+            break;
+        
+        case 'p':
+            // 0 pzt_min
+            // 1 pzt_max
+            // 2 scans_to_average
+            // 3 reaq_stepsize
+            // 4 fb_stepsize
+            // 5 unlocked_threshold
+            // 6 locked_threshold
+            // 7 pzt_offset_delta
+            int pnum = Serial.parseInt();
+            int val = Serial.parseInt();
+            switch (pnum){
+            case 0:
+                pzt_min = val;
+                break;
+            case 1:
+                pzt_max = val;
+                break;
+            case 2:
+                scans_to_average = val;
+                break;
+            case 3:
+                reaq_stepsize = val;
+                break;
+            case 4:
+                fb_stepsize = val;
+                break;
+            case 5:
+                unlocked_threshold = val;
+                break;
+            case 6:
+                locked_threshold = val;
+                break;
+            case 7:
+                pzt_offset_delta = val;
+                break;
+                    
+            
+            
+            }
+        }
+        
+        // not sure why flush() is broken...
+        // but then again so is this :/
+        while (Serial.available() > 0){
+            Serial.read();
+        }
+        processSM();
+    }
+}
+
+
+float process_scan(){
+
+// increment scan count
+      scan_counter++;
+      
+      uint16_t peak_heights[MAX_PEAK_CT];
+      uint8_t pk_height_idx = 0;
+      
+      uint16_t current_max;
+      
+      // stash peak maxima
+      for (int i = 0; i < pk_count; i++){
+        current_max = 0;
+        for (int j = 0; j < pt_count[i]; j++){
+            current_max = (buf[i][j] > current_max) ? buf[i][j] : current_max;
+        }
+        peak_heights[pk_height_idx++] = current_max;
+      }
+      
+      // calculate average
+      uint32_t sum = 0;
+      for (int i = 0; i < pk_height_idx; i++){
+        sum += peak_heights[i];
+      }
+      
+      // adjust pzt_min in case too close to turnaround.
+      // want to servo first peak at pzt_min + 300??
+      
+//       if (pk_count > 1){
+//           int pzt_offset = pk_position[0] - pzt_offset_delta;
+//           if (pzt_offset < 0){
+//             pzt_min = pk_position[0] + pzt_offset_delta;
+//           } else {
+//             pzt_min = pzt_offset;
+//           }
+//       }
+      return ((float) sum / pk_count);
+    
+}
+
+
+float old_average = 0.0;
+int polarity = 1;
+
+void loop() {
+
+    processSerial();
+   
+    
+    // if finished a scan, calculate average peak height
+    if (end_scan_flag){
+        // reset scan flag
+        end_scan_flag = 0;
+       // reject scans with bad peak count?
+        average_height +=  process_scan();
+        
+        if (scan_counter % scans_to_average == 0){
+            global_average = average_height/scans_to_average;
+            
+            if (isnan(global_average) && state != 0){
+                state = 2;        
+            }
+            
+            Serial.print(global_average);
+            Serial.print("\t");
+            Serial.print(fb_o);
+            Serial.print("\t");
+            Serial.print(state);
+            Serial.print("\t");
+            Serial.println(polarity);
+            
+            switch (state){
+            case 0:
+                // reset mode
+                break;
+            case 1:
+                // lock mode
+                if (unlocked_threshold < global_average && global_average <= locked_threshold){
+                    // if "locked" but marginal, step current to see which side
+                    old_average = global_average;
+                    fb_o += fb_stepsize*polarity;
+                    state = 11;
+                
+                    update_fb();
+                   
+                } else if(global_average < unlocked_threshold){
+                    state = 2;  // put in re-acquire mode
+                }
+                //otherwise, do nothing!
+                break;
+            
+            case 11:
+                // entered here from lock mode; sampling which side of lock region
+                if (global_average < old_average){
+                    // moving in wrong direction, flip polarity
+                    polarity = -1*polarity;
+                }
+                fb_o += fb_stepsize*polarity;
+                old_average = global_average;
+            
+            
+                if (global_average > locked_threshold){
+                    fb_o += fb_stepsize*polarity;
+                    state = 1;
+                } else if (global_average < unlocked_threshold){
+                    // messed up, try to reacquire lock
+                    state = 2;
+                }
+                update_fb();
+                break;
+            
+            case 2:
+                // need to re-acquire
+                if (global_average > unlocked_threshold){
+                    // lock has caught, so go into locking mode!
+                    state = 1;
+                } else {
+                    fb_o -= reaq_stepsize;
+                    update_fb();
+                }
+                break;
+            
+            
+        
+        
+            }
+        
+            // reset average height
+            average_height = 0.0;
+        }
+        
+        delay(1);
+        query_cavity();
+    } 
+}  
+
+
+       //  if (fb_flag == 1){
+//             if (average_height/scans_to_average >= 1450.0){ 
+//                 fb_flag = 2; // move to locked mode
+//                 Serial.print("Lock mode\t");
+//                 Serial.println(fb_o);
+//             } else {
+//                   Serial.print(fb_o);
+//             Serial.print("\t");
+//             Serial.println(global_ave);
+//                 fb_o -= df_fb;
+//                 analogWrite(DAC1, fb_o);
+//             }
+//         } else if (fb_flag == 2){
+//             // if in locked mode...
+//             switch (lock_sm){
+//             
+//                 case 0:
+// 
+//                 lock_hist[0] = global_ave;
+//                 lock_val[0] = fb_o;
+//                 
+//                 // only do something if at edge of range
+//                 if(lock_hist[0] < 1450 || isnan(lock_hist[0])){
+//                                   
+//                 
+//                 // sample up next time around
+//                 fb_o += 30;
+//                 analogWrite(DAC1, fb_o);
+//                 lock_sm = 1;
+//                 }
+//                 break;
+//                 
+//                 case 1:
+//        
+// 
+//                 lock_hist[1] = global_ave;
+//                 lock_val[1] = fb_o;
+//                 // sample down next time around
+//                 fb_o = lock_val[0] - 30;
+//                 analogWrite(DAC1, fb_o);
+//                 
+//                 lock_sm = 2;
+//                 break;
+//                 
+//                 case 2:
+//       
+//                 lock_hist[2] = global_ave;
+//                 lock_val[2] = fb_o;
+//       
+//                 Serial.print("lock\t");
+//                 Serial.print(fb_o+30);
+//                 Serial.print("\t");
+//                 Serial.print(lock_hist[2]);
+//                 Serial.print("\t");
+//                 Serial.print(lock_hist[0]);
+//                 Serial.print("\t");
+//                 Serial.println(lock_hist[1]);
+//               
+//                 
+//       
+//                 if(lock_hist[2] > (lock_hist[0]+5)){
+//                     Serial.println("decreasing");
+//                     //fb_o = lock_val[2];
+//                     fb_o = lock_val[0] - 60;
+//                 //fb_o -= 2*30;
+//                 analogWrite(DAC1, fb_o);
+//                 } else if (lock_hist[1] > (lock_hist[0] + 5)){
+//                     //fb_o = lock_val[1]; 
+//                     fb_o = lock_val[0] + 60;
+//                     analogWrite(DAC1, fb_o);
+//                     
+//                     Serial.println("increasing");
+//                     }else if (isnan(lock_hist[0])){
+//                         lock_sm = 0;
+//                
+//                         fb_flag = 1;
+//                         df_fb = 300;
+//                         fb_o = 4000;
+//                         Serial.println("relocking");
+//                     }
+//                     else {
+//                     fb_o = lock_val[0];
+//                     analogWrite(DAC1, fb_o);
+//                     //Serial.println("same");
+//                     } 
+//                 lock_sm = 0;
+//                 break;
+//                 
+//                 
+//             }
+//         }
+//         average_height = 0.0;
+//       }
---
+++ arduino/injection-ffwd/injection-init.h
@@ -0,0 +1,115 @@
+#include "Arduino.h"
+
+
+#define DEBUG 1
+
+#define PZT_RRR 15  // PZT rising ramp rate
+#define PZT_FRR -15 // PZT falling ramp rate
+#define PZT_TIMESTEP 100  // microseconds; dt between PZT waveform updates
+
+// these set when the ADC activates DMA. If the ADC value goes above HIGH_THRESHOLD, 
+// the DMA begins. If it then falls below LOW_THRESHOLD, it stops and will increment to 
+// the next buffer.
+#define LOW_THRESHOLD 930 
+#define HIGH_THRESHOLD 1000 
+
+// Number of scans to average over for deciding "peak height"
+// #define SCANS_TO_AVERAGE 10
+
+#define BUFFER_SIZE 1024    // ADC buffer for single peak (triggered by threshold above)
+#define MAX_PEAK_CT 16      // number of peaks to allocate memory for
+
+#define RAILED_OVERHEAD 100 // how close can we get to rails during locked mode?
+
+uint16_t buf[MAX_PEAK_CT][BUFFER_SIZE];  // allocate the buffer for ADC DMA
+
+
+
+// these keep track of how many points/peak and how many peaks were read during a given 
+// scan.
+volatile uint16_t pk_count = 0; // peak counter for given scan
+volatile uint16_t pt_count[MAX_PEAK_CT];  // number of points per peak
+volatile uint16_t pk_position[MAX_PEAK_CT]; // track voltage position of peak.
+                                        // if too close to zero, can skip
+
+volatile int16_t pzt_o = 0;  // piezo output voltage
+volatile int16_t pzt_min = 0;   // pzt min voltage
+volatile int16_t pzt_max = 4095;    // pzt max voltage
+
+uint8_t scans_to_average = 10;
+
+
+void ADC_Handler(){
+  // only run for right interrupt reason... ie, out of windowed range.
+  if ((adc_get_status(ADC) & ADC_ISR_COMPE) == ADC_ISR_COMPE){
+    
+    // toggle comparison mode. 
+    // ADC_EMR register 0b00 -> LOW mode, 0b01 -> HIGH mode
+    ADC->ADC_EMR ^= 0x01; 
+      
+    NVIC_ClearPendingIRQ(ADC_IRQn);
+    if (!(ADC->ADC_EMR & 0x01)){
+      // if rising, turn on DMA
+        REG_ADC_RPR = (uint32_t) buf[pk_count];  // set recieve pointer register
+        REG_ADC_RCR = BUFFER_SIZE;  // set recieve counter register
+        REG_ADC_PTCR = ADC_PTCR_RXTEN;  // enable DMA by setting RX Enable on Transfer Control Register
+        pk_position[pk_count] = pzt_o;  // start of peak
+    } else {
+        REG_ADC_PTCR = ADC_PTCR_RXTDIS;  // disable DMA
+        // store the number of points read in to DMA for given peak
+        pt_count[pk_count++] = BUFFER_SIZE - REG_ADC_RCR;
+    }
+  }
+}
+
+
+//ADC & DAC Configuration
+void adc_dac_setup(){
+  analogWriteResolution(12);    // 12 bits on DAC
+  
+  // ADC configuration
+  analogReadResolution(12);
+  adc_init(ADC, SystemCoreClock, ADC_FREQ_MAX, ADC_STARTUP_FAST);
+  adc_configure_timing(ADC, 0, ADC_SETTLING_TIME_3, 1);
+
+  ADC->ADC_CHER = ADC_CHER_CH7; // enable ADC on channel 7, which is A0 for DUE.
+  ADC->ADC_MR |= 0x80;  // ADC mode register; set free running bit (will sample as fast as possible)
+  adc_set_comparison_channel(ADC, ADC_CHANNEL_7);   // set up for comparison window interrupts 
+  adc_set_comparison_mode(ADC, ADC_EMR_CMPMODE_HIGH); // triggers interrupt when conversion > high threshold
+  adc_set_comparison_window(ADC, LOW_THRESHOLD, HIGH_THRESHOLD);  // LOW_THRESHOLD and HIGH_THRESHOLD are both const, so need to be defined at compile... see top of file.
+  adc_enable_interrupt(ADC, ADC_IER_COMPE); // turn on interrupt on comparison event
+  adc_start(ADC); // start the ADC!
+  
+  pmc_enable_periph_clk(ID_ADC); // power management controller; enables clock for ADC 
+  
+  // ADC Interrupt NVIC Enable; clear and start.
+  NVIC_DisableIRQ(ADC_IRQn);      
+  NVIC_ClearPendingIRQ(ADC_IRQn);
+  NVIC_SetPriority(ADC_IRQn, 0);
+  NVIC_EnableIRQ(ADC_IRQn);
+}
+
+
+typedef enum state_t { RESET, LOCKED, SAMPLE_UP, SAMPLE_DOWN, ACQUIRE, OFF } state_t; 
+typedef void (*state_callback_t)();
+
+typedef struct SM SM;
+
+struct SM {
+    state_t state;
+    SM * next_state;
+    state_callback_t callback;
+};
+
+SM * current_state;
+
+void run_sm(){
+    // run current state
+    (current_state->callback)();
+}
+
+
+
+
+
+
